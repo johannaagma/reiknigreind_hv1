38334d5a2028899e817e9600675fa497dcf2a6b0
Jóhanna Agnes <johannaagma@gmail.com>
2015-09-25 22:12:18 +0000

    Sækja gögn beint fra apis

--- hv1/global.R
+++ hv1/global.R
@@ -1,19 +1,26 @@
 library(shiny)
 library(googleVis)
+source("helperFunctions.R")
 
 smaprufa <- read.csv('litidtest.csv', sep=";")
 
 #==============================================
-
-source("helpFunctions.R")
-
-hjonavigslur_kvk <- read.csv("data/hjonavigslur.csv", skip=2, sep=";")
+# Hjónavígslur eftir aldri brúðhjóna 1971-2011
+#==============================================
+hjonavigslur_kvk <-getData("http://px.hagstofa.is/pxis/api/v1/is/Ibuar/fjolsk/Giftingar/MAN06101.px",
+                           list("Kyn"="1", "Ár"=as.character(c(8:48)), "Aldur"=as.character(c(1:10))))
 hjonavigslur_kvk <- hjonavigslur_kvk[,2:ncol(hjonavigslur_kvk)]
-hjonavigslur_kvk <- makeDataFrame(hjonavigslur_kvk)
+hjonavigslur_kvk <- fixColNames_age(hjonavigslur_kvk)
+hjonavigslur_kvk <- setUpFinalTable(hjonavigslur_kvk, "Hjónavígslur kvk")
 
-tekjur_kvk <- read.csv("data/tekjur.csv", skip=2, sep=";")
+#=====================================================
+# Meðalatvinnutekjur í aðalstarfi eftir kyni og aldri
+#=====================================================
+tekjur_kvk <- getData("http://px.hagstofa.is/pxis/api/v1/is/Samfelag/launogtekjur/3_tekjur/VIN07002.px",
+                      list("Landshlutar"="0", "Aldur"=as.character(c(1:14)),"Kyn"="2", "Ár"=c("*")))
 tekjur_kvk <- tekjur_kvk[,2:ncol(tekjur_kvk)]
 tekjur_kvk <- transposeTable(tekjur_kvk)
-tekjur_kvk <- renameColumn(tekjur_kvk, 1)
-tekjur_kvk <- makeDataFrame(tekjur_kvk)
+tekjur_kvk[,1] <- renameVector(as.character(tekjur_kvk[,1]))
+tekjur_kvk <- setUpFinalTable(tekjur_kvk, "Tekjur kvk")
+
 
---
+++ hv1/helperFunctions.R
@@ -0,0 +1,121 @@
+# Notkun: data = getData(url, dims)
+# Fyrir: ekkert
+# Eftir: data eru dataFrame af gögnunum frá url, með query-ið dims
+getData <- function(url, dims) {
+  table <- data.table(get_pxweb_data(
+    url=url, 
+    dims=dims, 
+    clean=FALSE
+  ))
+  return(as.data.frame(table))
+}
+
+# Notkun: newTable = setUpFinalTable(table, colName)
+# Fyrir: table er tafla þar sem fyrsti dálkurinn er ártal,
+#        og hinu dálkarnir eru aldurshópar. Þ.e.:
+#           aldur aldur aldur
+#       ár
+#       ár
+#       ár
+# Eftir: newTable er dataframe með Date(ártal), 
+#        Age(aldursbil) og colName(meginupplýsingarnar úr table)
+setUpFinalTable <- function(table, colName) {
+  Date <- numeric()
+  for(i in (1:nrow(table))) {
+    Date <- c(Date, rep(table$Ár[i], ncol(table)-1))
+  }
+  
+  Age <- rep(names(table)[2:ncol(table)], nrow(table))
+  
+  Data <- numeric()
+  for(i in (1:nrow(table))) {
+    for(j in (2:ncol(table))) {
+      Data <- c(Data, table[i,j])
+    }
+  }
+  dataFrame <- data.frame(Date, Age, Data)
+  colnames(dataFrame)[3] = colName
+  return(dataFrame)
+}
+
+# Notkun: newTable = transposeTable(table)
+# Fyrir: table lítur út svona:
+#        z  x x
+#        y
+#        y
+# Eftir: newTable lítur út svona:
+#        Ár y y
+#        x
+#        x
+#        Búið er að snúa töflunni við, og fyrsti dálkurinn
+#        fær nafnið "Ár"
+transposeTable <- function(table) {
+  firstColumn <- names(table)
+  newTable <- data.frame("Ár" = firstColumn[2:length(firstColumn)])
+  
+  for(i in(1:nrow(table))) {
+    newTable[, as.character(table[i,1])] <- as.numeric(table[i,2:ncol(table)])
+  }
+  return(newTable)
+}
+
+# Notkun: newTable = fixColNames_age(table)
+# Fyrir: titlarnir á dálkunum í table (frá og með dálk 2) eru á 
+#        forminu: "X15.19" og "X60.ára.og.eldri"
+# Eftir: búið er að breyta þessum titlum yfir á formið: "15-19 ára" og
+#        "60+ ára"
+fixColNames_age <- function(table) {
+  for(i in (2:ncol(table))) {
+    names(table)[i] <- getCorrectString(names(table)[i])
+  }
+  return(table)
+}
+
+# Notkun: newStr = getCorrectString(str)
+# Fyrir: str er á forminu: "X15.19" og "X60.ára.og.eldri"
+# Eftir: búið er að breyta string yfir á formið: "15-19 ára" og
+#        "60+ ára"
+getCorrectString <- function(str) {
+  interval <- strsplit(str, "\\.")
+  newString <- ""
+  if(length(interval[[1]]) == 2) {
+    newString <- paste0(interval[[1]][1], "- ára")
+  }
+  else if(length(interval[[1]]) == 3) {
+    newString <- paste0(interval[[1]][1], "-", interval[[1]][2]," ára")
+  }
+  else if (length(interval[[1]]) == 4) {
+    newString <- paste0(interval[[1]][1], "+ ára")
+  }
+  return(newString)
+}
+
+
+# Notkun: newTable = renameVector(vector)
+# Fyrir: gildin í vector eru strengir sem innihalda ".", t.d. 
+#        "Konur.2001" eða "Alls.lifandi.fædd.börn.35.39.ára"
+# Eftir: búið er að setja fyrsta gildið sem gefur eftir fyrstu tilkomu
+#        tölustafs, t.d. "2001" og "35.39.ára"
+renameVector <- function(vector) {
+  list = c() # eða = character()
+  for(i in (1:length(vector))) {
+    idx = getIndexOfFirstNumber(vector[i])
+    temp = substring(vector[i], idx)
+    list[i] = temp
+  }
+  return(list)
+}
+
+# Notkun: idx = getIndexOfFirstNumber(str)
+# Fyrir: str inniheldur amk eina tölu í sér
+# Eftir: idx segir til um hvar fyrst tölustafurinn í str er
+#        staðsettur
+getIndexOfFirstNumber <- function(str) {
+  idx  = c()
+  for(i in(0: nchar(str))) {
+    temp = lapply(strsplit(str, ''), function(x) which(x == toString(i)))
+    temp = temp[[1]][1]
+    idx = c(idx, temp)
+  }
+  return(min(idx, na.rm=TRUE))
+}
