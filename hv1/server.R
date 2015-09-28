2947d31c5e3958c6c6b6055c47908cd5b20f2ab6
davidsteinar <davidsteinar@gmail.com>
2015-09-28 12:56:46 +0000

    réttir ásar

--- hv1/server.R
+++ hv1/server.R
@@ -1,8 +1,16 @@
+library(shiny)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  
   output$gvMotion <- renderGvis({
-    gvisMotionChart(allData,
-                    idvar="Age", 
-                    timevar="Date")
-    })
+    
+  gvisMotionChart(allData,
+                  idvar="Age",
+                  timevar="Date",
+                  yvar="Liabilities of individuals [Million ISK]",
+                  xvar="Assets of individuals [Million ISK]",
+                  sizevar="Total annual income [Million ISK]")
+  
+                                })
   })
