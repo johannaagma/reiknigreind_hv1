cc373615ceb098e54d199c879f9443f9baa9acc4
davidsteinar <davidsteinar@gmail.com>
2015-09-28 12:43:41 +0000

    lysing komin

--- hv1/UI.R
+++ hv1/UI.R
@@ -1,10 +1,12 @@
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Data by agegroup"),
+  headerPanel("Financial status between agegroups"),
   sidebarPanel(
     p(class = "text-muted",
-      paste("here comes description"))
+      paste("This multivariate graph shows relations between financial status and 
+            several social attributes within agegroups over time.
+            The graph is interactive and you can change the axis at will."))
   ),
   mainPanel(tableOutput("gvMotion"))
 ))
