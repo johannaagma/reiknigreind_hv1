39d61fe1b9eba808ab429f1d5cbc99eb5a216972
Jóhanna Agnes <johannaagma@gmail.com>
2015-10-12 18:34:26 +0000

    Bæta við heimild

--- hv1/UI.R
+++ hv1/UI.R
@@ -1,12 +1,22 @@
 shinyUI(pageWithSidebar(
   
-  # Application title
   headerPanel("Financial status between agegroups"),
   sidebarPanel(
-    p(class = "text-muted",
-      paste("This multivariate graph shows relations between financial status and 
-            several social attributes within agegroups over time.
-            The graph is interactive and you can change the axis at will."))
+    strong("Description:"),
+    p(class = "text-muted", paste("This multivariate graph shows relations between financial 
+                                  status and several social attributes within agegroups over 
+                                  time. The graph is interactive and you can change the axis 
+                                  at will.")),
+    
+    strong("Authors:"),
+    p(class = "text-muted", paste("Davíð Steinar Ásgrímsson, Jóhanna Agnes Magnúsdóttir")),
+    
+    strong("Source:"),
+    p(class = "text-muted", paste("hagstofan.is, datamarket.com")),
+    
+    strong("Github:"),
+    p(class = "text-muted", paste("https://github.com/johannaagma/reiknigreind_hv1"))
   ),
   mainPanel(tableOutput("gvMotion"))
 ))
+
